d639cf27d8ce8094facdc7a265da86c8379acbc0
mimyutt <mutiakhofifaturr@gmail.com>
2026-05-11 10:47:41 +0700

    menambahkan atribut dan metode

--- AgregasiIbu.cpp
+++ AgregasiIbu.cpp
@@ -2,4 +2,19 @@
 #define IBU_H
 #include <vector>
 
-class ibu
+class ibu {
+public: 
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "ibu \"" << nama << "\" ada\n";
+
+    }
+    ~ibu() {
+         cout << "ibu \"" << nama << "\" tidak ada\n";
+
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+}
